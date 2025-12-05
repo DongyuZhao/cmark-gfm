7a7d5539f497e29b0665597ac08ea99462ef6c20
Dongyu Zhao <dy.zhao@outlook.com>
2025-12-05 14:29:01 +0800

    Add math syntax extension

---
+++ extensions/math.c
@@ -0,0 +1,147 @@
+#include "math.h"
+#include <parser.h>
+#include <render.h>
+#include <chunk.h>
+
+cmark_node_type CMARK_NODE_INLINE_MATH;
+cmark_node_type CMARK_NODE_MATH_BLOCK;
+
+static cmark_node *match_math(cmark_syntax_extension *self, cmark_parser *parser,
+                              cmark_node *parent, unsigned char character,
+                              cmark_inline_parser *inline_parser) {
+  if (character != '$') {
+    return NULL;
+  }
+
+  cmark_chunk *chunk = cmark_inline_parser_get_chunk(inline_parser);
+  int start = cmark_inline_parser_get_offset(inline_parser);
+  const unsigned char *data = chunk->data + start;
+  int remaining = (int)(chunk->len - (bufsize_t)start);
+
+  if (remaining < 3) {
+    return NULL;
+  }
+
+  int delimiter_length = data[1] == '$' ? 2 : 1;
+  int i = delimiter_length;
+
+  while (i < remaining) {
+    if (data[i] == '$' && data[i - 1] != '\\') {
+      int closing_length = 1;
+      while (i + closing_length < remaining && data[i + closing_length] == '$') {
+        closing_length++;
+      }
+
+      if (closing_length >= delimiter_length) {
+        bufsize_t content_length = (bufsize_t)(i - delimiter_length);
+        unsigned char *content_start = (unsigned char *)(data + delimiter_length);
+        cmark_node_type node_type =
+            delimiter_length == 1 ? CMARK_NODE_INLINE_MATH : CMARK_NODE_MATH_BLOCK;
+
+        cmark_node *node = cmark_node_new_with_mem(node_type, parser->mem);
+        cmark_node_set_syntax_extension(node, self);
+
+        char *tmp = (char *)parser->mem->calloc(1, content_length + 1);
+        memcpy(tmp, content_start, content_length);
+        cmark_chunk_set_cstr(parser->mem, &node->as.literal, tmp);
+        parser->mem->free(tmp);
+        node->start_line = node->end_line =
+            cmark_inline_parser_get_line(inline_parser);
+        node->start_column =
+            cmark_inline_parser_get_column(inline_parser) - delimiter_length;
+
+        cmark_inline_parser_set_offset(
+            inline_parser, start + i + closing_length);
+        return node;
+      }
+
+      i += closing_length;
+      continue;
+    }
+    ++i;
+  }
+
+  return NULL;
+}
+
+static const char *get_type_string(cmark_syntax_extension *extension,
+                                   cmark_node *node) {
+  if (node->type == CMARK_NODE_INLINE_MATH) {
+    return "inlineMath";
+  }
+  if (node->type == CMARK_NODE_MATH_BLOCK) {
+    return "mathBlock";
+  }
+  return "<unknown>";
+}
+
+static int can_contain(cmark_syntax_extension *extension, cmark_node *node,
+                       cmark_node_type child_type) {
+  return 0;
+}
+
+static int contains_inlines(cmark_syntax_extension *extension,
+                            cmark_node *node) {
+  return node->type == CMARK_NODE_INLINE_MATH;
+}
+
+static void commonmark_render(cmark_syntax_extension *extension,
+                              cmark_renderer *renderer, cmark_node *node,
+                              cmark_event_type ev_type, int options) {
+  bool entering = ev_type == CMARK_EVENT_ENTER;
+  const char *delim =
+      node->type == CMARK_NODE_INLINE_MATH ? "$" : "$$";
+
+  if (entering) {
+    renderer->out(renderer, node, delim, false, LITERAL);
+  } else {
+    renderer->out(renderer, node, delim, false, LITERAL);
+  }
+}
+
+static void html_render(cmark_syntax_extension *extension,
+                        cmark_html_renderer *renderer, cmark_node *node,
+                        cmark_event_type ev_type, int options) {
+  bool entering = ev_type == CMARK_EVENT_ENTER;
+  const char *delim =
+      node->type == CMARK_NODE_INLINE_MATH ? "$" : "$$";
+
+  if (entering) {
+    cmark_strbuf_puts(renderer->html, delim);
+    cmark_strbuf_put(renderer->html, node->as.literal.data,
+                     node->as.literal.len);
+    cmark_strbuf_puts(renderer->html, delim);
+    renderer->skip = true;
+  }
+}
+
+static void plaintext_render(cmark_syntax_extension *extension,
+                             cmark_renderer *renderer, cmark_node *node,
+                             cmark_event_type ev_type, int options) {
+  if (ev_type == CMARK_EVENT_ENTER) {
+    cmark_renderer_esc(renderer, node->as.literal.data, node->as.literal.len);
+  }
+}
+
+cmark_syntax_extension *create_math_extension(void) {
+  cmark_syntax_extension *ext = cmark_syntax_extension_new("math");
+  cmark_llist *special_chars = NULL;
+
+  cmark_syntax_extension_set_get_type_string_func(ext, get_type_string);
+  cmark_syntax_extension_set_can_contain_func(ext, can_contain);
+  cmark_syntax_extension_set_contains_inlines_func(ext, contains_inlines);
+  cmark_syntax_extension_set_commonmark_render_func(ext, commonmark_render);
+  cmark_syntax_extension_set_plaintext_render_func(ext, plaintext_render);
+  cmark_syntax_extension_set_html_render_func(ext, html_render);
+
+  CMARK_NODE_INLINE_MATH = cmark_syntax_extension_add_node(1);
+  CMARK_NODE_MATH_BLOCK = cmark_syntax_extension_add_node(1);
+
+  cmark_syntax_extension_set_match_inline_func(ext, match_math);
+
+  cmark_mem *mem = cmark_get_default_mem_allocator();
+  special_chars = cmark_llist_append(mem, special_chars, (void *)'$');
+  cmark_syntax_extension_set_special_inline_chars(ext, special_chars);
+
+  return ext;
+}
